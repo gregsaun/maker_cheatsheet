f79db2588585bcf7b4745ee15aa41e3e85ed9fe2
gregsaun <gregoire.saunier@ekunn.com>
2021-02-19 15:27:13 +0100

    use real PI value and add comments

--- cad_design/fusion360_scripts/Create3DPParams.py
+++ cad_design/fusion360_scripts/Create3DPParams.py
@@ -3,10 +3,12 @@
 
 import adsk.core, adsk.fusion, traceback
 
+# List of the parameters you would like to add
+# Format: ['name', 'value', 'unit', 'comment'],
 paramsToAdd = [
     ['layer_height',     '0.2',                                   'mm', ''],
     ['e_width',          '0.45',                                  'mm', 'extrusion_width'],
-    ['e_spacing',        'e_width - layer_height * (1 - 3.14/4)', 'mm', 'extrusion spacing according to PrusaSlicer default settings'],
+    ['e_spacing',        'e_width - layer_height * (1 - PI / 4)', 'mm', 'extrusion spacing according to PrusaSlicer default settings'],
     ['num_perimeters',   '4',                                     '',   'number of perimeters'],
     ['num_tops_bottoms', '5',                                     '',   'number of top and bottom layers'],
     ['m3_hole',          '3.3',                                   'mm', 'M3 hole diameter'],
@@ -24,18 +26,19 @@ def run(context):
         app = adsk.core.Application.get()
         ui  = app.userInterface
 
+        # Get the active design
         design = adsk.fusion.Design.cast(app.activeProduct)
         if not design:
             ui.messageBox('No active Fusion design')
             return
 
+        # Parse parameters one by one and add them to the active design
         params = design.userParameters
         for paramNum in range(len(paramsToAdd)) :
             print(paramsToAdd[paramNum][0])
             params.add(paramsToAdd[paramNum][0], adsk.core.ValueInput.createByString(paramsToAdd[paramNum][1]), paramsToAdd[paramNum][2], paramsToAdd[paramNum][3])
-        
-        ui.messageBox('Parameters have been created successfully')
 
+        ui.messageBox('Parameters have been created successfully')
 
     except:
         if ui:
