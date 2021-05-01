9a965687ec7005da1d89c255b77f5357ce7cc7cd
gregsaun <gregoire.saunier@ekunn.com>
2021-05-01 15:50:38 +0200

    add parameter wall_thickness_double, useful for selective infill and small wall design

--- cad_design/fusion360_scripts/Create3DPParams.py
+++ cad_design/fusion360_scripts/Create3DPParams.py
@@ -6,18 +6,19 @@ import adsk.core, adsk.fusion, traceback
 # List of the parameters you would like to add
 # Format: ['name', 'value', 'unit', 'comment'],
 paramsToAdd = [
-    ['layer_height',     '0.2',                                   'mm', ''],
-    ['e_width',          '0.45',                                  'mm', 'extrusion_width'],
-    ['e_spacing',        'e_width - layer_height * (1 - PI / 4)', 'mm', 'extrusion spacing according to PrusaSlicer default settings'],
-    ['num_perimeters',   '4',                                     '',   'number of perimeters'],
-    ['num_tops_bottoms', '5',                                     '',   'number of top and bottom layers'],
-    ['m3_hole',          '3.3',                                   'mm', 'M3 hole diameter'],
-    ['m3_head',          '5.8',                                   'mm', 'M3 screw ISO4762 head diameter'],
-    ['m3_thread',        '2.7',                                   'mm', 'M3 self threading hole diameter'],
-    ['m3_hex_nut',       '6.2',                                   'mm', 'M3 hex nut ISO4032 inscribed diameter'],
-    ['m3_square_nut',    '5.5',                                   'mm', 'M3 square nut DIN562 pocket width'],
-    ['m5_hole',          '5.3',                                   'mm', 'M5 hole diameter'],
-    ['m5_head',          '10',                                    'mm', 'M5 screw ISO4762 head diameter'],
+    ['layer_height',          '0.2',                                   'mm', ''],
+    ['e_width',               '0.45',                                  'mm', 'extrusion_width'],
+    ['e_spacing',             'e_width - layer_height * (1 - PI / 4)', 'mm', 'extrusion spacing according to PrusaSlicer default settings'],
+    ['num_perims',            '4',                                     '',   'number of perimeters'],
+    ['num_tops_bottoms',      '5',                                     '',   'number of top and bottom layers'],
+    ['m3_hole',               '3.3',                                   'mm', 'M3 hole diameter'],
+    ['m3_head',               '5.8',                                   'mm', 'M3 screw ISO4762 head diameter'],
+    ['m3_thread',             '2.7',                                   'mm', 'M3 self threading hole diameter'],
+    ['m3_hex_nut',            '6.2',                                   'mm', 'M3 hex nut ISO4032 inscribed diameter'],
+    ['m3_square_nut',         '5.5',                                   'mm', 'M3 square nut DIN562 pocket width'],
+    ['m5_hole',               '5.3',                                   'mm', 'M5 hole diameter'],
+    ['m5_head',               '10',                                    'mm', 'M5 screw ISO4762 head diameter'],
+    ['wall_thickness_double', 'floor(( e_width + ( num_perims * 2 - 1 ) * e_spacing ) * 100) / 100', 'mm', 'wall thickness made with double number of perimeters (without infill)'],
 ]
 
 def run(context):
